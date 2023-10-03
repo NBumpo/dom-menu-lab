0f6e6b9b53767f81b50dfc17d7a81caf6be3c2e3
nathanaelchojnacki <nbumpo11@gmail.com>
2023-10-02 23:14:11 -0500

    done

--- js/app.js
+++ js/app.js
@@ -46,8 +46,76 @@ mainEl.style.backgroundColor = ('var(--main-bg)')
 mainEl.appendChild(hOneEl)
 hOneEl.innerHTML = "SEI Rocks!";
 document.querySelector('main').classList.add('flex-ctr')
-
+//from here is where I needed the solutions page
+//
+//listens for click us click is not an A tag return
 topMenuEl.addEventListener('click',function(e){
     e.preventDefault();
-    
+    const link = e.target
+    if (link.tagName !== 'A') return;
+    console.log(link.textContent)
 })
+    topMenuLinks.forEach(function(link) {
+
+    })
+    
+
+if (link.classList.contains('active')) {
+    link.classList.remove('active')
+    showingSubMenu = false
+    subMenuEl.style.top = '0'
+    return
+}
+
+ // Task 5.4 iterates through links and removes active state
+ topMenuLinks.forEach(function(link) {
+  link.classList.remove('active');
+});
+// Task 5.5
+link.classList.add('active');
+// Task 5.6
+// finds linkObj returns linkobj if strict equals link textContent
+const linkData = menuLinks.find(function(linkObj) {
+  return linkObj.text === link.textContent;
+});
+showingSubMenu = 'subLinks' in linkData;
+// Task 6.4
+// Task 5.7
+if (showingSubMenu) {
+  buildSubMenu(linkData.subLinks);
+  subMenuEl.style.top = '100%';
+} else {
+  subMenuEl.style.top = '0';
+  mainEl.innerHTML = '<h1>about</h1>';
+}
+
+
+// Task 5.8
+function buildSubMenu(subLinks) {
+subMenuEl.innerHTML = '';
+subLinks.forEach(function(link) {
+  const linkEl = document.createElement('a');
+  linkEl.setAttribute('href', link.href);
+  linkEl.textContent = link.text;
+  subMenuEl.appendChild(linkEl);
+});
+}
+
+// Task 6.0 repeated from earlier
+subMenuEl.addEventListener('click', function(evt) {
+evt.preventDefault();
+const link = evt.target;
+if (link.tagName !== 'A') return;
+console.log(link.textContent);
+// Task 6.1
+showingSubMenu = false;
+subMenuEl.style.top = '0';
+// Task 6.2
+topMenuLinks.forEach(function(link) {
+  link.classList.remove('active');
+});
+// Task 6.3
+mainEl.innerHTML = `<h1>${link.textContent}</h1>`;
+});
+
+
